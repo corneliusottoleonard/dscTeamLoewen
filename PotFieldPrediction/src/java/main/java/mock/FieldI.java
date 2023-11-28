6b3c22c2f54343168c9d07c4dff455fbf4265628
gregor <gregor.endress@web.de>
2023-11-28 15:01:17 +0100

    add interfaces to convert csv to javaobject

---
+++ PotFieldPrediction/src/java/main/java/mock/FieldI.java
@@ -0,0 +1,11 @@
+package Mock;
+
+import java.util.List;
+
+public interface FieldI {
+    // Object which stores a List of
+    // CoordinateI objects
+    public List<CoordinateI> getCoordinates();
+    public void setCoordinates(String filePath);
+
+}
