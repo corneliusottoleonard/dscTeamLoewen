0d0f9be1d8959e39e0134b4c2e6958fe3c827041
Mohamed Tarek Gazzah <m.gazzah@hm.edu>
2023-12-03 21:55:38 +0100

    add grpc Service for fit and predict

--- PotFieldPrediction/src/java/main/grpc_server/grpcClient.java
+++ PotFieldPrediction/src/java/main/grpc_server/grpcClient.java
@@ -1,8 +1,26 @@
 package grpc_server;
 
+import grpc_generated.Grpc;
 import grpc_generated.PredictionServiceGrpc;
+import io.grpc.stub.StreamObserver;
 
 public class grpcClient extends PredictionServiceGrpc.PredictionServiceImplBase {
+
+    @Override
+    public void fit(Grpc.FieldSequence request, StreamObserver<Grpc.FitResponse> responseObserver) {
+        // Implement your fit logic here
+        Grpc.FitResponse response = Grpc.FitResponse.newBuilder().build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void predict(Grpc.PredictRequest request, StreamObserver<Grpc.PredictionResponse> responseObserver) {
+        // Implement your predict logic here
+        Grpc.PredictionResponse response = Grpc.PredictionResponse.newBuilder().build();
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
 
 }
 
